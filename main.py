7cc0299fa65b207e727eeff48314b44bacc31dd5
YooN03 <wlsrb0309@gmail.com>
2025-06-02 01:13:37 +0900

    끄아악

--- main.py
+++ main.py
@@ -1,11 +1,58 @@
 from wordbook import *
 from game import *
-words = {}
-print('기능 선택\n' \
-'1.단어 저장\n' \
-'2.단어장\n' \
-'3.게임\n' \
-'4.나가기\n') 
-# 단어 저장 = add_word() 불러오고, 단어장 = show_word() 불러오고, 
-# 게임 = play_game()불러오고, 나가기 = 나갔다는 문구랑 함께 main.py종료 할 수 있게 plz
-a = input("1, 2, 3, 4 입력: ")
+from pathlib import Path
+
+
+def main():
+    while True:
+          
+        print('기능 선택\n' \
+        '1.단어 저장\n' \
+        '2.단어장\n' \
+        '3.게임\n' \
+        '4.나가기\n') 
+    
+        a = input("1, 2, 3, 4 입력: ")
+        show_word = {}
+        #형변환 점검       
+        try:
+            answer = int(a)
+        except ValueError:
+            print("1 이상 4 이하 정수 값 하나만 입력하세요!")
+            continue
+    
+        if answer == 1:
+            print('단어를 입력하세요(언제든 \'exit\'를 입력해 단어 입력을 끝낼 수 있습니다.)\n')
+            input_word()
+            continue
+        elif answer == 2:
+            while True:
+                num = int(input('불러올 단어의 갯수를 입력하세요: '))
+                show_word = show_wordbook(num)
+                if not show_word:
+                    continue
+                elif a =='null':
+                    break
+                else:
+                    print(show_word)
+                    break
+            continue
+        elif answer == 3:
+            if not show_word:
+                print('단어장을 먼저 불러오세요')
+                continue
+            else:
+                play_game(show_word)
+        elif answer == 4:
+            print('프로그램을 종료합니다')
+            return 
+        else:
+            print("1 이상 4 이하 정수 값 하나만 입력하세요!")
+            continue
+
+    # 단어 저장 = add_word() 불러오고, 단어장 = show_word() 불러오고, 
+    # 게임 = play_game()불러오고, 나가기 = 나갔다는 문구랑 함께 main.py종료 할 수 있게 plz
+
+if __name__ == "__main__":
+	main()
+
--- wordbook.py
+++ wordbook.py
@@ -1,12 +1,78 @@
-def add_word(words_list): #영단어랑 뜻 딕셔너리 형태로 저장함.
+import json
+import random
+
+def input_word(): #영단어랑 뜻 딕셔너리 형태로 저장함.
+    word = {}
     while True:
-        a = input('word: ')
+        a = input('word: ').strip().lower()
         if a == 'exit':
             break
         b = input('meaning: ')
         if b == 'exit':
             break
-        words_list[a] = b
+        word[a] = b
+        
+#단어장 생성 or 업데이트        
+    answer = input('단어장 파일을 생성하셨나요?: Y/N ').strip().upper()
+        
+    if answer == 'Y':
+        while True:
+            note_path = input('파일명을 입력하세요(\'파일명.json\'의 형태로 입력하세요):').strip()
+            if note_path[-5:] != '.json':
+                print('파일명 작성법을 지키세요!!')
+                continue
 
-def show_wordbook(words_list):
-    print(words_list)
+            update_wordbook(word, note_path)
+            return
+
+    elif answer == 'N':
+        while True:
+            note_path = input('생성할 파일명을 입력하세요(\'파일명.json\'의 형태로 입력하세요):').strip()
+            if note_path[-5:] != '.json':
+                print('파일명 작성법을 지키세요!!')
+                continue
+
+            with open(note_path, "w", encoding="utf-8") as f:
+                json.dump(word, f, ensure_ascii=False, indent=4)
+            return
+
+
+def update_wordbook(word_list, file_path):
+#이전 단어 불러오기     
+    with open(file_path, "r", encoding="utf-8") as f:
+        past_word = json.load(f)
+
+#새로운 단어들과 이전 단어들 간 중복 단어 제거 
+    overlap_dict = [word_list, past_word]
+    pair_set = set()
+    for d in overlap_dict:
+        pair_set.update(d.items())
+    
+#단어 업데이트   
+    with open(file_path, "w", encoding="utf-8") as f:
+        json.dump(dict(pair_set), f, ensure_ascii=False, indent=4)
+    return
+
+def show_wordbook(num_of_word):
+     while True:
+    #파일 열기
+        note_path = input('파일명을 입력하세요(\'파일명.json\'의 형태로 입력하세요):').strip()
+        if note_path[-5:] != '.json':
+            print('파일명 작성법을 지키세요!!')
+            continue
+    #파일 존재 유무 검사        
+        try:
+            with open(note_path, "r", encoding="utf-8") as f:
+                show_word = json.load(f)
+        except FileNotFoundError:
+            print('파일이 존재하지 않습니다')
+            return 'null' 
+    #값 반환
+        number = len(show_word)
+        if num_of_word > number:
+            print(f'불러올 단어가 너무 많습니다. 현재 {number}개의 단어가 저장되어 있습니다.')
+            return {}
+        else:
+            return dict(random.sample(list(show_word.items()), num_of_word))
+
+
